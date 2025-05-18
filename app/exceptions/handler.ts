a9fb30b5fd620dab69b53e0165a117865572b02a
Ernestas Kluonis <ekgame1@gmail.com>
2025-05-18 23:34:14 +0300

    Implemented error pages

--- app/exceptions/handler.ts
+++ app/exceptions/handler.ts
@@ -1,6 +1,7 @@
 import app from '@adonisjs/core/services/app'
 import { HttpContext, ExceptionHandler } from '@adonisjs/core/http'
-import type { StatusPageRange, StatusPageRenderer } from '@adonisjs/core/types/http'
+import type { HttpError, StatusPageRange, StatusPageRenderer } from '@adonisjs/core/types/http'
+import { errors } from '@adonisjs/lucid';
 
 export default class HttpExceptionHandler extends ExceptionHandler {
   /**
@@ -14,19 +15,40 @@ export default class HttpExceptionHandler extends ExceptionHandler {
    * codes. You might want to enable them in production only, but feel
    * free to enable them in development as well.
    */
-  protected renderStatusPages = app.inProduction
+  protected renderStatusPages = true;
 
   /**
    * Status pages is a collection of error code range and a callback
    * to return the HTML contents to send as a response.
    */
   protected statusPages: Record<StatusPageRange, StatusPageRenderer> = {
-    '404': (error, { view }) => {
-      return view.render('pages/errors/not_found', { error })
+    '404': (error, ctx) => {
+      return this.customErrorHandler(error, ctx, () => {
+        return ctx.view.render('pages/errors/not_found', { error });
+      });
     },
-    '500..599': (error, { view }) => {
-      return view.render('pages/errors/server_error', { error })
+    '500..599': (error, ctx) => {
+      return this.customErrorHandler(error, ctx, () => {
+        return ctx.view.render('pages/errors/server_error', { error });
+      });
     },
+  }
+
+  private customErrorHandler(error: HttpError, ctx: HttpContext, htmlRenderer: () => Promise<string>) {
+    if (error instanceof errors.E_ROW_NOT_FOUND && error.model) {
+      error.message = `${error.model.name} not found.`;
+    }
+
+    if (ctx.request.accepts(['json'])) {
+      return this.renderErrorAsJSON(error, ctx);
+    }
+
+    return htmlRenderer();
+  }
+
+  protected isDebuggingEnabled(ctx: HttpContext): boolean {
+    const isServerError = ctx.response.getStatus() >= 500 && ctx.response.getStatus() <= 599;
+    return this.debug && isServerError;
   }
 
   /**
